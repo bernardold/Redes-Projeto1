2267e266d8cd23ecdcdb77e4f540a07067f0a293
Giovani Ortolani Barbosa <giovaniortolani@users.noreply.github.com>
2017-06-03 20:18:19 -0300

    Add files via upload

--- server.c
+++ server.c
@@ -5,103 +5,166 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
-#include <pthread.h>
 
-#include "socketsFunction.h"
+#include "socketsFunctions.h"
 
 //#define MINHAPORTA 8081   /* Porta que os usuarios irão se conectar*/
+#define BACKLOG 5     /* Quantas conexões pendentes serão indexadas */
 #define MAXBUFFERSIZE 256
-#define N 2
 
-struct ThreadArgs { /* Structure of arguments to pass to client thread */
-    int clntSock; /* socket descriptor for client */
-};
+char result[256];
+float info[2] = {0.0};
+int opt = -1;
 
-char client0[MAXBUFFERSIZE];
-char client1[MAXBUFFERSIZE];
+void connectionHandler(int);
 
 int main(int argc, char **argv)
 {
-    int serverSocket[N], clientSocket[N];  /* escuta em serverSocket, nova conexão
+    int serverSocket, clientSocket;  /* escuta em serverSocket, nova conexão
                                    em clientSocket */
-    int portnum[] = {51111, 51112};
-    struct sockaddr_in server_address[N];    /* informação do meu endereco */
-    struct sockaddr_in client_address[N]; /* informação do endereco do conector */
-    unsigned int clientLength;
-    char buffer[2][MAXBUFFERSIZE] = {'\0', '\0'};
-    char id;
+    int portnum;
+    struct sockaddr_in server_address;    /* informação do meu endereco */
+    struct sockaddr_in client_address; /* informação do endereco do conector */
+    int clientLength;
+    char buffer[MAXBUFFERSIZE];
+    int pid;
 
-    pthread_t threadID[N];
-    struct ThreadArgs *threadArgs;
-    int argu[N];
+    int i = 0, opt = -1;
 
-    int i = 0;
-
-    if(argc != 1) 
+    if(argc != 2) 
     {
-        fprintf(stderr, "Usage: %s\n", argv[0]);
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    for(i = 0; i < N; i++)
+    if((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
     {
-        serverSocket[i] = createSocket();
-
-        bzero((char *) &server_address, sizeof(server_address)); /* Zera o resto da estrutura */
-        server_address[i].sin_family = AF_INET;
-        server_address[i].sin_port = htons(portnum[i]);
-        server_address[i].sin_addr.s_addr = INADDR_ANY; // ou inet_addr("127.0.0.1") /* coloca IP automaticamente */
-
-        bindToAddress(serverSocket[i], &server_address[i]);
-
-        listenToConnections(serverSocket[i]);
+        error("ERROR opening socket");
     }
 
+    portnum = atoi(argv[1]);
+
+    bzero((char *) &server_address, sizeof(server_address)); /* Zera o resto da estrutura */
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(portnum);
+    server_address.sin_addr.s_addr = INADDR_ANY; // ou inet_addr("127.0.0.1") /* coloca IP automaticamente */
+
+    if(bind(serverSocket, (struct sockaddr *) &server_address, sizeof(struct sockaddr)) == -1) 
+    {
+        error("ERROR on binding socket");
+    }
+
+    if(listen(serverSocket, BACKLOG) < 0) 
+    {
+        error("ERROR on listening");
+    }
 
     printf("Servidor ativo!\n");
 
-    clientLength = sizeof(client_address[0]);
-    i = 0;
+    clientLength = sizeof(client_address);
     while(1) 
     {   
-        while(i < N)
+            if((clientSocket = accept(serverSocket, (struct sockaddr *) &client_address, &clientLength)) < 0)
+            {
+                error("ERROR on accept");
+            }
+            printf("Servidor: chegando conexão de %d\n", inet_ntoa(client_address.sin_addr));
+            i++;
+
+            pid = fork();
+            if(pid < 0) 
+            {   
+                error("ERROR on fork");
+            }
+            else if(pid == 0) // apenas os processos filhos entram aqui
+            {   
+                close(serverSocket);
+                // HANDLE INICIO
+                connectionHandler(clientSocket);
+                // HANDLE FIM
+                printf("Servidor: terminada a conexao com %d\n", inet_ntoa(client_address.sin_addr));
+                exit(0);
+            }
+            printf("i: %d\n", i);
+            if(i == 2)
+            {
+                while(1)
+                {
+                    printf("entrou!\n");
+                    scanf("%d", &opt);
+                }
+            }
+            close(clientSocket);
+            while(waitpid(-1, NULL, WNOHANG) > 0); /* Limpa os processos filhos */
+    }
+    return 0;
+}
+
+void connectionHandler(int clientSocket)
+{
+    char buffer[MAXBUFFERSIZE];
+    char id[2];
+
+    if(write(clientSocket, "Seja bem-vindo!\n", 16) < 0)
+    {
+        perror("ERROR on writing to socket");
+        close(clientSocket);
+        exit(EXIT_FAILURE);
+    }
+    while(1)
+    {
+        bzero(buffer, MAXBUFFERSIZE);  // zera o buffer
+        printf("opt: %d\n", opt);
+        if(opt == 0)
         {
-            clientSocket[i] = acceptConnection(serverSocket[i], &client_address[i], &clientLength);
-            printf("Servidor: chegando conexão de %s\n", inet_ntoa(client_address[i].sin_addr));
-            if(writeToSocket(clientSocket[i], "Connection established with server!\n") < 0)
+            if(writeToSocket(clientSocket, "C0") < 0)
             {
                 perror("ERROR on writing to socket");
-                close(clientSocket[i]);
+                close(clientSocket);
                 exit(EXIT_FAILURE);
             }
-            i++;
-        }
-        //else{
-        while(1)
-        {
-            for(i = 0; i < N; i++)
-            {
-                //bzero(buffer[i], MAXBUFFERSIZE);  // zera o buffer
-                if(readFromSocket(clientSocket[i], buffer[i], MAXBUFFERSIZE) < 0) 
-                {
-                    error("ERROR reading from socket");
-                }
-                id = buffer[i][0];
-                if(id == '0')
-                {
-                    strcpy(client0, buffer[0] + 1);
-                }
-                else if(id == '1')
-                {
-                    strcpy(client1, buffer[1] + 1);
-                }
-                //usleep(1000000);
-                //system("clear");
-                printf("Client 0: %s\nClient 1: %s\n", client0, client1);
-            }
-        }   
-    }
 
-    return 0;
+            if(readFromSocket(clientSocket, buffer, MAXBUFFERSIZE) < 0) 
+            {
+                error("ERROR reading from socket");
+            }
+
+            id[0] = buffer[0];
+            id[1] = buffer[1];
+            if(!strcmp(id, "C0"))
+            {    
+                printf("Client 0 aqui. ");
+                printf("Here is the message: %s\n", buffer);
+            }
+            opt = -1;
+        }
+        else if(opt == 1)
+        {
+            if(writeToSocket(clientSocket, "C1") < 0)
+            {
+                perror("ERROR on writing to socket");
+                close(clientSocket);
+                exit(EXIT_FAILURE);
+            }
+
+            if(readFromSocket(clientSocket, buffer, MAXBUFFERSIZE) < 0) 
+            {
+                error("ERROR reading from socket");
+            }
+
+            id[0] = buffer[0];
+            id[1] = buffer[1];
+            if(!strcmp(id, "C1"))
+            {    
+                printf("Client 1 aqui. ");
+                printf("Here is the message: %s\n", buffer);
+            }
+            opt = -1;
+        }
+        else if(opt == 5)
+        {
+            exit(0);
+        }
+    }
+    close(clientSocket);   
 }
